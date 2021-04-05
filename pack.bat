d2624ed110f776c89dd581b97f6f7949c04fe22a
Itamar Amith <itamaram@gmail.com>
2021-04-06 09:32:25 +1000

    props and targets

--- pack.bat
+++ pack.bat
@@ -1,3 +1,3 @@
 for %%p in (Itamaram.SpecFlow.Plugin.Base Itamaram.Csv.SpecFlowPlugin Itamaram.Excel.SpecFlowPlugin) do (
-	nuget pack %%p\%%p.csproj -Build -Prop Configuration=Release -version %1
+	nuget pack %%p\%%p.csproj -Build -Prop Configuration=Release -Version %1 -Verbosity detailed
 )
