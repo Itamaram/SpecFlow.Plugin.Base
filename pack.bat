3414784e110a41e604cd2516a4e4450354c1eb1d
Itamar Amith <itamaram@gmail.com>
2021-04-06 10:50:29 +1000

    Fix up props and targets

--- pack.bat
+++ pack.bat
@@ -1,3 +1,3 @@
 for %%p in (Itamaram.SpecFlow.Plugin.Base Itamaram.Csv.SpecFlowPlugin Itamaram.Excel.SpecFlowPlugin) do (
-	dotnet pack %%p\%%p.csproj -c Release -p:Version=%1
+	dotnet pack %%p\%%p.csproj -c Release -p:Version=%1 -o .
 )
